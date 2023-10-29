35a83b15a513d3c0e063aad26d04d82c03d5e53f
tcodes317 <tcodes317@gmail.com>
2023-10-29 10:07:54 -0700

    i

---
+++ app.js
@@ -0,0 +1,30 @@
+const http=require("http");
+const fs=require("fs");
+const url=require("url");
+const contact=fs.readFileSync("./project/contact.html", "utf-8");
+const page_=fs.readFileSync("./project/404.html", "utf-8");
+const home=fs.readFileSync("./project/home.html", "utf-8");
+const gallery=fs.readFileSync("./project/gallery.html", "utf-8");
+const about=fs.readFileSync("./project/about.html", "utf-8");
+const server=http.createServer();
+server.on("request", (req, res)=>{
+    let {query, pathname: path}=url.parse(req.url, true);
+    if("/home" === path.toLocaleLowerCase() || "/" === path.toLocaleLowerCase()){
+        res.end(home, "utf-8");
+    }
+    else if("/gallery" === path.toLocaleLowerCase()){
+        res.end(gallery)
+    }
+    else if("/contact" === path.toLocaleLowerCase()){
+        res.end(contact);
+    }
+    else if("/about" === path.toLocaleLowerCase()){
+        res.end(about);
+    }
+    else if(req.url!=home || req.url!=gallery || req.url!=about || req.url!=contact){
+        res.end(page_)
+    }
+})
+server.listen(1000, "127.0.0.1", ()=>{
+    console.log("Connected!")
+})
---
+++ replaceHtml.js
@@ -0,0 +1,11 @@
+module.exports=function(template, product){
+    let output=template.replace("%id%", product.id);
+    output=output.replace("%name%", product.name);
+    output=output.replace("%age%", product.age);
+    output=output.replace("%DOB%", product.DOB);
+    output=output.replace("%occupation%", product.occupation);
+    output=output.replace("%id%", product.id);
+    output=output.replace("%src%", product.src)
+
+    return output;
+}
