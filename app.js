5683109bb98d7012341b2018d5addcb23a9e46e0
tcodes317 <tcodes317@gmail.com>
2023-10-29 10:19:22 -0700

    ii

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http=require("http");
 const fs=require("fs");
 const url=require("url");
+const replaceHtml = require("./replaceHtml");
 const contact=fs.readFileSync("./project/contact.html", "utf-8");
+const dCont=fs.readFileSync("./project/dCont.html", "utf-8");
 const page_=fs.readFileSync("./project/404.html", "utf-8");
 const home=fs.readFileSync("./project/home.html", "utf-8");
 const gallery=fs.readFileSync("./project/gallery.html", "utf-8");
 const about=fs.readFileSync("./project/about.html", "utf-8");
+const dBase=JSON.parse(fs.readFileSync("database/database.json", "utf-8"));
 const server=http.createServer();
 server.on("request", (req, res)=>{
     let {query, pathname: path}=url.parse(req.url, true);
@@ -13,7 +16,7 @@ server.on("request", (req, res)=>{
         res.end(home, "utf-8");
     }
     else if("/gallery" === path.toLocaleLowerCase()){
-        res.end(gallery)
+        res.end(gallery.replace("%gallery page%", ))
     }
     else if("/contact" === path.toLocaleLowerCase()){
         res.end(contact);
